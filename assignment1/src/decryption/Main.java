c9f7220def45c807dd05b0f80fc90b5b71d65769
Matthew Sagen <matthewsagen@Matthews-MacBook-Pro.local>
2019-01-17 14:41:25 -0700

    almost done with decrpytion

--- assignment1/src/decryption/Main.java
+++ assignment1/src/decryption/Main.java
@@ -1,31 +1,79 @@
 package decryption;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		PrintWriter writer = null;
+		//use PrintWriter to output results to Sagen_Matthew.txt
+		try {
+			writer = new PrintWriter("src/decryption/Sagen_Matthew.txt", "UTF-8");	
+		} 
+		catch (FileNotFoundException | UnsupportedEncodingException e) {
+			e.printStackTrace();	
+		}
+		//input: An array to hold input file text.
+		//output: an array to hold output file text.
 		ArrayList<String> input = new ArrayList<String>();
+		ArrayList<String> output = new ArrayList<String>();
+		//get the input from the input file
 		getInput(input);
-		
+		//for each word in the input array, decrypt the word.
+		for(String word : input) {
+			analyzeWord(word, output);
+		}
+		//for each word in the output array, output the decrypted message to the output file
+		for(String word: output) {
+			System.out.println(word);
+			writer.println(word);
+		}
+		writer.close();
 	}
 	
+	public static void analyzeWord(String word, ArrayList<String> output) {
+		String finalWord;								//string to add to output array
+		char[] originalWord = word.toCharArray();		//the original word changed to a character array
+		char[] newWord = new char[originalWord.length];	//create a char array to represent the decrypted word
+		//for each character in the original text, decrypt the character.
+		for(int i = 0; i < originalWord.length; i++) {
+			int val = (int) originalWord[i];
+			if(originalWord[i] == ',' || originalWord[i] == '.') {
+				newWord[i] = originalWord[i];
+			}
+			else {
+				if(val - 10 < 97) {
+					val = (122 - (val - 97));
+				}
+				else {
+					val = (val - 10) ;
+				}
+				newWord[i] = (char) val;
+			}			
+		}
+		finalWord = new String(newWord);
+		output.add(finalWord);
+	}
+	//helper method to get input text from input.txt
 	public static void getInput(ArrayList<String> input) {
 		Scanner scanner = null;
-		 try{
-			 scanner = new Scanner(new File("src/decryption/input.txt"));
-	     }
-		 //throws error if the file can't be located.
-		 catch(Exception e){
-			 System.out.println("File not found.");
-			 System.exit(0);
-		 }
-		 //add data to input list.
-		 while(scanner.hasNext()){
-			 input.add(scanner.next()); 
-	     }
-		 scanner.close();
+		try{
+			scanner = new Scanner(new File("src/decryption/input.txt"));
+	    }
+		//throws error if the file can't be located.
+		catch(Exception e){
+			System.out.println("File not found.");
+			System.exit(0);
+		}
+		//add data to input list.
+		while(scanner.hasNext()){
+			input.add(scanner.next()); 
+	    }
+		scanner.close();
 	}
 }
