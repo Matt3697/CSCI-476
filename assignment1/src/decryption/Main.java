2e7bfb0447aff94eee447287d769a67e951e3f16
Matthew Sagen <matthewsagen@matthews-mbp.home>
2019-01-17 18:36:17 -0700

    finished

--- assignment1/src/decryption/Main.java
+++ assignment1/src/decryption/Main.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -29,9 +30,13 @@ public class Main {
 			analyzeWord(word, output);
 		}
 		//for each word in the output array, output the decrypted message to the output file
-		for(String word: output) {
+		/*for(String word: output) {
 			System.out.println(word);
 			writer.println(word);
+		}*/
+		for(int i = 0; i < input.size(); i++) {
+			System.out.println(input.get(i) + " --> " + output.get(i));
+			writer.println(input.get(i) + " --> " + output.get(i));
 		}
 		writer.close();
 	}
@@ -40,6 +45,33 @@ public class Main {
 		String finalWord;								//string to add to output array
 		char[] originalWord = word.toCharArray();		//the original word changed to a character array
 		char[] newWord = new char[originalWord.length];	//create a char array to represent the decrypted word
+		HashMap<Character,Character> map = new HashMap<Character,Character>();
+		map.put('a', 'm');//done
+		map.put('b', 'l');//done
+		map.put('c', 'k');//done
+		map.put('d', 'j');//done
+		map.put('e', 'i');//done
+		map.put('f', 'h');//done
+		map.put('g', 'g');//done
+		map.put('h', 'f');//done
+		map.put('i', 'e');//done
+		map.put('j', 'd');//done
+		map.put('k', 'c');//done
+		map.put('l', 'b');//done
+		map.put('m', 'a');//done
+		map.put('n', 'z');//done
+		map.put('o', 'y');//done
+		map.put('p', 'x');//done
+		map.put('q', 'w');//done
+		map.put('r', 'v');//done
+		map.put('s', 'u');//done
+		map.put('t', 't');//done
+		map.put('u', 's');//done
+		map.put('v', 'r');//done
+		map.put('w', 'q');//done
+		map.put('x', 'p');//done
+		map.put('y', 'o');//done
+		map.put('z', 'n');//done
 		//for each character in the original text, decrypt the character.
 		for(int i = 0; i < originalWord.length; i++) {
 			int val = (int) originalWord[i];
@@ -47,13 +79,7 @@ public class Main {
 				newWord[i] = originalWord[i];
 			}
 			else {
-				if(val - 10 < 97) {
-					val = (122 - (val - 97));
-				}
-				else {
-					val = (val - 10) ;
-				}
-				newWord[i] = (char) val;
+				newWord[i] = map.get(originalWord[i]);
 			}			
 		}
 		finalWord = new String(newWord);
